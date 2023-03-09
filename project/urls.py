ea504204bf7823011499db2ca7b8cda5a03ea867
Kostja P <kpavlovsky@users.noreply.github.com>
2023-03-09 20:37:03 +0100

    Initial commit

---
+++ project/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+
+urlpatterns = [
+
+    path('', include('main.urls')),
+    path('accounts/', include('allauth.urls')),
+    path(settings.ADMIN_URL, admin.site.urls),
+    # API Schema:
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
