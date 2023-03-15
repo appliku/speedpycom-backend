ad087800eb6b5b3b48a5130e08ad35275dbd7be0
Kostja P <kpavlovsky@users.noreply.github.com>
2023-03-15 13:50:13 +0100

    login / logout

--- main/serializers/auth.py
+++ main/serializers/auth.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class LogoutSerializer(serializers.Serializer):
-    status = serializers.CharField(readonly=True)
+    status = serializers.CharField(read_only=True)
